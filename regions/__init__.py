33d565edbe81a9a8ba6a1ea01a3f88f2921624bf
Yannick Schwartz <yannick.schwartz@cea.fr>
2012-11-16 13:17:37 +0100

    mv utils.py to helpers.py ; mv function from roi.py in new utils.py

--- regions/__init__.py
+++ regions/__init__.py
@@ -2,4 +2,4 @@
 __version__ = '0.1'
 
 from .roi import Atlas, Parcellation
-from .utils import harvard_oxford_atlas, mni_atlas, juelich_atlas
+from .helpers import harvard_oxford_atlas, mni_atlas, juelich_atlas
--- regions/roi.py
+++ regions/roi.py
@@ -5,29 +5,8 @@ import pylab as pl
 import nibabel as nb
 
 import nipy.labs.viz as viz
-from nipy.labs.datasets import VolumeImg
 
-
-def resample(source, target, interpolation='continuous', return_affine=False):
-    source, source_affine = source
-    target, target_affine = target
-
-    input_image = VolumeImg(source[:],
-                            source_affine,
-                            'arbitrary',
-                            interpolation=interpolation
-                            )
-
-    resampled_image = input_image.as_volume_img(target_affine, target.shape)
-
-    if return_affine:
-        return resampled_image.get_data(), resampled_image.get_affine()
-    else:
-        return resampled_image.get_data()
-
-
-def atlas_mean(R_voxels, weights):
-    return np.mean(np.multiply(R_voxels, weights), axis=1)
+from .utils import resample, atlas_mean, check_float_approximation
 
 
 class Atlas(object):
@@ -316,15 +295,6 @@ class Atlas(object):
     def save(self, location):
         img = nb.Nifti1Image(self.A.astype('float32'), self.affine)
         nb.save(img, location)
-
-
-def check_float_approximation(P, mask):
-    lmap = {}
-    for label in np.unique(P[mask]):
-        lmap[label] = np.round(label)
-
-    for label in lmap:
-        P[P == label] = lmap[label]
 
 
 class Parcellation(object):
